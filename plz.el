fc8159a6a9bf56d70614931bc50a7adf62707967
Adam Porter <adam@alphapapa.net>
2023-04-13 15:54:16 -0500

    Release: v0.5

--- plz.el
+++ plz.el
@@ -5,7 +5,7 @@
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; Maintainer: Adam Porter <adam@alphapapa.net>
 ;; URL: https://github.com/alphapapa/plz.el
-;; Version: 0.5-pre
+;; Version: 0.5
 ;; Package-Requires: ((emacs "26.3"))
 ;; Keywords: comm, network, http
 
